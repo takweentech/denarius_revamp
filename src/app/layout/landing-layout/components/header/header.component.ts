fa9dc9c54c0a357f45c4395f824d0366aece5821
muhammed loay <70457297+muhammed-loay@users.noreply.github.com>
2025-03-20 22:12:03 +0200

    layout

--- src/app/layout/landing-layout/components/header/header.component.ts
+++ src/app/layout/landing-layout/components/header/header.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component, TemplateRef, inject } from "@angular/core";
+import { RouterLink } from "@angular/router";
+import {
+  NgbModal,
+  NgbCollapseModule,
+  NgbOffcanvas,
+} from "@ng-bootstrap/ng-bootstrap";
+import { TranslatePipe, TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-header',
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  selector: "app-header",
+  standalone: true,
+  imports: [RouterLink, CommonModule, NgbCollapseModule, TranslatePipe],
+  templateUrl: "./header.component.html",
 })
 export class HeaderComponent {
+  private modalService = inject(NgbModal);
+  private translateService = inject(TranslateService);
+  private offcanvasService = inject(NgbOffcanvas);
+  isCollapsed = false;
+  resetPassword = false;
+  navbarToggler: boolean = false;
+  email: string = "support@denarius.com";
+  lang: string = this.translateService.currentLang;
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onLogin() {}
+
+  onSignUp() {}
+
+  openSidebar(content: TemplateRef<any>) {
+    this.offcanvasService.open(content, {
+      position: "bottom",
+      panelClass: "vh-70 rounded-top-1",
+    });
+  }
+
+  onLangChange() {}
 }
