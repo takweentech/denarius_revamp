0e46b35a172b4056fa6e715a872feff03a081797
muhammed loay <70457297+muhammed-loay@users.noreply.github.com>
2025-03-22 19:10:55 +0200

    display opportunities in homepage

--- src/app/presentation/landing/pages/homepage/components/opportunities/opportunities.component.ts
+++ src/app/presentation/landing/pages/homepage/components/opportunities/opportunities.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { InvestmentOpportunitiesComponent } from "../../../../../../shared/components/investment-opportunities/investment-opportunities.component";
 
 @Component({
-  selector: 'app-opportunities',
-  imports: [],
-  templateUrl: './opportunities.component.html',
-  styleUrl: './opportunities.component.scss'
+  selector: "app-opportunities",
+  imports: [InvestmentOpportunitiesComponent],
+  templateUrl: "./opportunities.component.html",
+  styleUrl: "./opportunities.component.scss",
 })
-export class OpportunitiesComponent {
-
-}
+export class OpportunitiesComponent {}
