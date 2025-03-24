92abd21c51a02e3b59e9c490376b03982782aa3e
zatattou <z.atattou@takweentech.sa>
2025-03-24 15:56:22 +0000

    save homepage integration progress

--- src/app/presentation/landing/pages/homepage/components/faq/faq.component.ts
+++ src/app/presentation/landing/pages/homepage/components/faq/faq.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-faq',
-  imports: [],
+  imports: [NgbAccordionModule],
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.scss'
 })
 export class FaqComponent {
-
+  items = ['First', 'Second', 'Third'];
 }
