741df61a070c09df8feca5ed49077e2e4795577b
muhammed loay <70457297+muhammed-loay@users.noreply.github.com>
2025-04-06 03:13:03 +0200

    add content to i18n

--- src/app/presentation/landing/pages/about-us/components/management/management.component.ts
+++ src/app/presentation/landing/pages/about-us/components/management/management.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { TranslateModule } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-management',
-  imports: [],
-  templateUrl: './management.component.html',
-  styleUrl: './management.component.scss'
+  selector: "app-management",
+  imports: [TranslateModule],
+  templateUrl: "./management.component.html",
+  styleUrl: "./management.component.scss",
 })
-export class ManagementComponent {
-
-}
+export class ManagementComponent {}
