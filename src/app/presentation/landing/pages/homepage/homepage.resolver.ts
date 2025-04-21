cbe4a400cad657b0de3d89a9528933f06d168a81
muhammed loay <70457297+muhammed-loay@users.noreply.github.com>
2025-04-21 22:02:50 +0200

    fix compliance alignment + add ar content

--- src/app/presentation/landing/pages/homepage/homepage.resolver.ts
+++ src/app/presentation/landing/pages/homepage/homepage.resolver.ts
@@ -1,10 +1,14 @@
 import { ResolveFn } from "@angular/router";
 import { StrapiService } from "../../../../core/strapi/strapi.service";
 import { inject } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
 export const homepageResolver: ResolveFn<boolean> = (route, state) => {
   const strapiService = inject(StrapiService);
-  return strapiService.getContentByPage(
-    "/homepage?populate=hero&populate=hero.media&populate=license&populate=cta&populate=cta.box&populate=cta.image&populate=compliance&populate=compliance.box&populate=faq&populate=faq.point&populate=faq.question&populate=opportunities&populate=partners"
-  );
+  const translate = inject(TranslateService);
+  const currentLang = translate.currentLang || translate.defaultLang || "en";
+
+  const url = `/homepage?locale=${currentLang}&populate=hero&populate=hero.media&populate=license&populate=cta&populate=cta.box&populate=cta.image&populate=compliance&populate=compliance.box&populate=faq&populate=faq.point&populate=faq.question&populate=opportunities&populate=partners`;
+
+  return strapiService.getContentByPage(url);
 };
