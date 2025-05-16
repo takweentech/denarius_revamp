3eaeb0518c339d3563cb119d7cffd06c9588e396
zatattou <z.atattou@takweentech.sa>
2025-05-16 18:09:20 +0100

    set default lang to ar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    defaultLang: 'en',
+    defaultLang: 'ar',
     apiUrl: 'https://sharikekapi.takweentech.sa/api/v1',
     cmsUrl: 'https://sharikekcms.paramej.com/api',
     cmsAssetsUrl: 'https://sharikekcms.paramej.com/',
