8cbda6f2736e77257b81b5189adfcbee6625a29a
muhammed loay <70457297+muhammed-loay@users.noreply.github.com>
2025-06-18 00:38:22 +0300

    fix mobile number, nationality, creation date in profile

--- src/app/core/models/user.ts
+++ src/app/core/models/user.ts
@@ -1,176 +1,170 @@
 export interface User {
-    investorId: number;
-    name: string;
-    emailConfirmed: boolean;
-    level: number;
-    levelName: string | null;
-    type: number;
-    typeName: string | null;
-    lastRequestUpgradeStatus: any;
-    requestUpgradeId: any;
-    lastRequestUpgradeStatusName: any;
-    idNumber: number;
-    gender: number;
-    genderName: string;
-    bankName: string | null;
-    iban: string | null;
-    personalIban: string | null;
-    ibanbalance: number;
-    totalWritingMoney: number;
-    totalStocks: number;
-    totalWriting: number;
-    bankTitle: string | null;
-    image: string | null;
-    needUpdate: boolean;
-    lastUpdateDate: string;
-    extraIncomeId: number | null;
-    extraIncomeText: string | null;
-    isForeign: boolean;
-    ibanFund: string | null;
-    personalIbanFund: string | null;
-    ibanFundBalance: number;
-    needUpdateAssets: boolean;
-    needUpdateGeneral: boolean;
-    commercialRegistryFile: string | null;
-    companyPolicy: string | null;
-    establishmentContract: string | null;
-    shareholdersDecision: string | null;
-    resolutionPartners: string | null;
-    boardDecision: string | null;
-    ibanSukuk: string | null;
-    ibanSukukBalance: number;
-    totalWritingMoneyFund: number;
-    totalStocksFund: number;
-    totalWritingFund: number;
-    totalWritingMoneySukuk: number;
-    totalStocksSukuk: number;
-    totalWritingSukuk: number;
-    termesCondition: boolean;
-    acknowledgement: boolean;
-    agreeAccount: boolean;
-    locked: boolean;
-    email: string | null;
-    idExpiryYear: number;
-    idExpiryMonth: number;
-    idExpiryDay: number;
-    birthYear: number;
-    birthMonth: number;
-    birthDay: number;
-    employmentStatus: string;
-    address: string | null;
-    commercialRegistrationNo: string | null;
-    endDateYear: number;
-    endDateMonth: number;
-    endDateDay: number;
-    startDateYear: number;
-    startDateMonth: number;
-    startDateDay: number;
-    phoneNumber: string | null;
-    id: number;
-    isComplete: boolean | null;
-    completePercent: number;
+  investorId: number;
+  name: string;
+  emailConfirmed: boolean;
+  level: number;
+  levelName: string | null;
+  type: number;
+  typeName: string | null;
+  lastRequestUpgradeStatus: any;
+  requestUpgradeId: any;
+  lastRequestUpgradeStatusName: any;
+  idNumber: number;
+  gender: number;
+  genderName: string;
+  bankName: string | null;
+  iban: string | null;
+  personalIban: string | null;
+  ibanbalance: number;
+  totalWritingMoney: number;
+  totalStocks: number;
+  totalWriting: number;
+  bankTitle: string | null;
+  image: string | null;
+  needUpdate: boolean;
+  lastUpdateDate: string;
+  extraIncomeId: number | null;
+  extraIncomeText: string | null;
+  isForeign: boolean;
+  ibanFund: string | null;
+  personalIbanFund: string | null;
+  ibanFundBalance: number;
+  needUpdateAssets: boolean;
+  needUpdateGeneral: boolean;
+  commercialRegistryFile: string | null;
+  companyPolicy: string | null;
+  establishmentContract: string | null;
+  shareholdersDecision: string | null;
+  resolutionPartners: string | null;
+  boardDecision: string | null;
+  ibanSukuk: string | null;
+  ibanSukukBalance: number;
+  totalWritingMoneyFund: number;
+  totalStocksFund: number;
+  totalWritingFund: number;
+  totalWritingMoneySukuk: number;
+  totalStocksSukuk: number;
+  totalWritingSukuk: number;
+  termesCondition: boolean;
+  acknowledgement: boolean;
+  agreeAccount: boolean;
+  locked: boolean;
+  email: string | null;
+  idExpiryYear: number;
+  idExpiryMonth: number;
+  idExpiryDay: number;
+  birthYear: number;
+  birthMonth: number;
+  birthDay: number;
+  employmentStatus: string;
+  address: string | null;
+  commercialRegistrationNo: string | null;
+  endDateYear: number;
+  endDateMonth: number;
+  endDateDay: number;
+  startDateYear: number;
+  startDateMonth: number;
+  startDateDay: number;
+  phoneNumber: string | null;
+  id: number;
+  isComplete: boolean | null;
+  completePercent: number;
 }
 
-
-
 export interface UserProfileData {
-    userProfile: UserProfile;
-    investmentAccount: InvestmentAccount;
-    financialSummary: FinancialSummary;
-    recentTransactions: any[];
-    financialDistribution: any;
-    investmentPerformance: InvestmentPerformance;
+  userProfile: UserProfile;
+  investmentAccount: InvestmentAccount;
+  financialSummary: FinancialSummary;
+  recentTransactions: any[];
+  financialDistribution: any;
+  investmentPerformance: InvestmentPerformance;
 }
 
 export interface UserPersonalData {
-    maritalStatus: number,
-    familyMembersCount: number,
-    educationLevel: number,
-    employmentStatus: number,
-    jobTitle: number,
-    yearsOfExperience: number,
-    annualIncome: number,
-    netWealth: number
+  maritalStatus: number;
+  familyMembersCount: number;
+  educationLevel: number;
+  employmentStatus: number;
+  jobTitle: number;
+  yearsOfExperience: number;
+  annualIncome: number;
+  netWealth: number;
 }
 
-export interface UserWalletData {
-}
+export interface UserWalletData {}
 
 export interface UserInvestmentStatisticsData {
-    totalInvestments: number;
-    totalProfits: number;
-    totalExpectedProfits: number;
-    totalDeposits: number;
-    totalWithdraws: number;
-    totalInvestmentCount: number;
+  totalInvestments: number;
+  totalProfits: number;
+  totalExpectedProfits: number;
+  totalDeposits: number;
+  totalWithdraws: number;
+  totalInvestmentCount: number;
 }
 
-
-
 export interface UserBasicProfileData {
-    fullName: string;
-    idNumber: string;
-    nationality: string | null;
-    mobileNumber: string;
-    idExpiryDate: string; // Consider using Date if it's properly formatted
-    startingDate: string; // Consider using Date if it's properly formatted
-    district: number;
-    streetName: string;
-    city: string;
-    postalCode: string;
-    additionalCode: string;
-    shortAddress: string;
+  fullName: string;
+  idNumber: string;
+  nationality: string | null;
+  mobileNumber: string;
+  idExpiryDate: string; // Consider using Date if it's properly formatted
+  startingDate: string; // Consider using Date if it's properly formatted
+  district: number;
+  streetName: string;
+  city: string;
+  postalCode: string;
+  additionalCode: string;
+  shortAddress: string;
 }
 
 export interface UserBankData {
-    bankId: number;
-    accountBeneficiaryName: string;
-    iban: string;
+  bankId: number;
+  accountBeneficiaryName: string;
+  iban: string;
 }
 
 export interface UserInvestmentData {
-    riskTolerance: number;
-    investmentExperience: number;
-    investmentHorizon: number;
-    investmentGoal: number;
-    isBeneficiary: number;
+  riskTolerance: number;
+  investmentExperience: number;
+  investmentHorizon: number;
+  investmentGoal: number;
+  isBeneficiary: number;
 }
 
-
 export interface UserProfile {
-    fullName: string;
-    profileImageUrl: string;
-    accountType: string;
-    nationalId: string;
-    nationality: string | null;
-    startDate: string;
-    expiryDate: string;
+  mobileNumber: string;
+  fullName: string;
+  profileImageUrl: string;
+  accountType: string;
+  nationalId: string;
+  nationality: string | null;
+  startDate: string;
+  expiryDate: string;
 }
 
 export interface InvestmentAccount {
-    bankName: string;
-    accountHolderName: string;
-    accountNumber: string | null;
-    iban: string | null;
-    balance: number;
-    openingDate: string;
-    expiryDate: string;
+  bankName: string;
+  accountHolderName: string;
+  accountNumber: string | null;
+  iban: string | null;
+  balance: number;
+  openingDate: string;
+  expiryDate: string;
 }
 
 export interface FinancialSummary {
-    totalDeposits: number;
-    totalInvestments: number;
-    totalProfits: number;
-    expectedProfits: number;
+  totalDeposits: number;
+  totalInvestments: number;
+  totalProfits: number;
+  expectedProfits: number;
 }
 
-
 export interface UserPassword {
-    currentPassword: string,
-    newPassword: string,
-    confirmedPassword: string
+  currentPassword: string;
+  newPassword: string;
+  confirmedPassword: string;
 }
 
 export interface InvestmentPerformance {
-    performanceData: any[];
+  performanceData: any[];
 }
--- src/app/presentation/admin/pages/settings/components/profile/profile.component.ts
+++ src/app/presentation/admin/pages/settings/components/profile/profile.component.ts
@@ -1,9 +1,17 @@
 import { Component, inject, OnInit, signal } from "@angular/core";
 import { TranslatePipe } from "@ngx-translate/core";
-import { UserBasicProfileData, UserProfileData } from "../../../../../../core/models/user";
+import {
+  UserBasicProfileData,
+  UserProfileData,
+} from "../../../../../../core/models/user";
 import { TokenService } from "../../../../../../core/services/token.service";
 import { DatePipe } from "@angular/common";
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
 import { ProfileService } from "../../../../../../data/profile.service";
 import { finalize, takeUntil } from "rxjs";
 import { BaseComponent } from "../../../../../../core/base/base.component";
@@ -28,11 +36,26 @@ export class ProfileComponent extends BaseComponent implements OnInit {
 
   ngOnInit(): void {
     this.initForm();
-    this.profileService.getBasicPersonalInformation().pipe(takeUntil(this.destroy$)).subscribe({
-      next: (response) => {
-        this.initForm(response.data);
-      }
-    })
+
+    this.profileService
+      .getBasicPersonalInformation()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          this.initForm(response.data);
+
+          // Map additional user details from API
+          this.user.userProfile = {
+            ...this.user.userProfile,
+            fullName: response.data.fullName,
+            mobileNumber: response.data.mobileNumber,
+            nationalId: response.data.idNumber,
+            expiryDate: response.data.idExpiryDate,
+            nationality: response.data.nationality,
+            startDate: response.data.startingDate,
+          };
+        },
+      });
   }
 
   initForm(data?: UserBasicProfileData) {
@@ -42,30 +65,40 @@ export class ProfileComponent extends BaseComponent implements OnInit {
       city: [data?.city, Validators.required],
       postalCode: [data?.postalCode, Validators.required],
       additionalCode: [data?.additionalCode, Validators.required],
-      shortAddress: "TEMP"
-    })
+      shortAddress: "TEMP",
+    });
   }
 
   onSave() {
     if (this.form.invalid) {
       this.form.markAllAsTouched();
-      return
-    };
+      return;
+    }
     this.loading.set(true);
 
-    this.profileService.saveBasicPersonalInformation(this.form.value).pipe(
-      finalize(() => this.loading.set(false)),
-      takeUntil(this.destroy$)).subscribe({
+    this.profileService
+      .saveBasicPersonalInformation(this.form.value)
+      .pipe(
+        finalize(() => this.loading.set(false)),
+        takeUntil(this.destroy$)
+      )
+      .subscribe({
         next: (response) => {
           if (response.status == 200) {
-            this.toastService.show({ text: "Address was successfully saved", classname: 'bg-success text-light', icon: 'fa-circle-check' });
+            this.toastService.show({
+              text: "Address was successfully saved",
+              classname: "bg-success text-light",
+              icon: "fa-circle-check",
+            });
           } else {
-            this.toastService.show({ text: response.message, classname: 'bg-danger text-light', icon: 'fa-circle-exclamation' });
+            this.toastService.show({
+              text: response.message,
+              classname: "bg-danger text-light",
+              icon: "fa-circle-exclamation",
+            });
           }
         },
-        error: (error) => {
-
-        }
-      })
+        error: (error) => {},
+      });
   }
 }
