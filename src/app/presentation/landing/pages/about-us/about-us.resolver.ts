c8d0ed5b4b1211d202a6968411e55cac19a54a60
zatattou <z.atattou@takweentech.sa>
2025-06-17 18:34:55 +0100

    make team members dynamic

--- src/app/presentation/landing/pages/about-us/about-us.resolver.ts
+++ src/app/presentation/landing/pages/about-us/about-us.resolver.ts
@@ -8,7 +8,7 @@ export const aboutResolver: ResolveFn<boolean> = (route, state) => {
   const translate = inject(TranslateService);
   const currentLang = translate.currentLang || translate.defaultLang || "en";
 
-  const url = `/about?locale=${currentLang}&populate=header&populate=overview&populate=overview.box&populate=values.item&populate=members.member&populate=members&populate=management&populate=management.member&populate=management.structure&populate=risk_notice&populate=compliance&populate=compliance.box`;
+  const url = `/about?locale=${currentLang}&populate=header&populate=overview&populate=overview.box&populate=values.item&populate=risk_notice&populate=compliance&populate=compliance.box&populate[0]=members&populate[1]=members.members&populate[2]=members.members.image&populate[3]=management&populate[4]=management.members&populate[5]=management.members.image&populate[6]=management.structure`;
 
   return strapiService.getContentByPage(url);
 };
--- src/app/presentation/landing/pages/about-us/components/management/management.component.ts
+++ src/app/presentation/landing/pages/about-us/components/management/management.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from "@angular/core";
 import { TranslateModule } from "@ngx-translate/core";
 import { BaseComponent } from "../../../../../../core/base/base.component";
 import { ActivatedRoute } from "@angular/router";
+import { environment } from "../../../../../../../environments/environment";
 
 @Component({
   selector: "app-management",
@@ -12,9 +13,11 @@ import { ActivatedRoute } from "@angular/router";
 export class ManagementComponent extends BaseComponent implements OnInit {
   private readonly activatedRoute = inject(ActivatedRoute);
   content = this.activatedRoute.snapshot.data["content"]["management"];
+  CMS_ASSETS_URL = environment.cmsAssetsUrl;
+
   constructor() {
     super();
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 }
