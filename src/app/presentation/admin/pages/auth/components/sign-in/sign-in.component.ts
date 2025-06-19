bc0c2b9ed4e4b1b27c3e175885267b87ad2e6bca
muhammed loay <70457297+muhammed-loay@users.noreply.github.com>
2025-06-19 17:31:27 +0300

    translate err msg

--- src/app/presentation/admin/pages/auth/components/sign-in/sign-in.component.ts
+++ src/app/presentation/admin/pages/auth/components/sign-in/sign-in.component.ts
@@ -1,20 +1,25 @@
-import { Component, inject, signal } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
-import { WEB_ROUTES } from '../../../../../../core/constants/routes.constants';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { TokenService } from '../../../../../../core/services/token.service';
-import { RegistrationApiService } from '../../../../../../data/registration.service';
-import { finalize, takeUntil } from 'rxjs';
-import { BaseComponent } from '../../../../../../core/base/base.component';
-import { ToastService } from '../../../../../../shared/components/toast/toast.service';
-import { ProfileService } from '../../../../../../data/profile.service';
-import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { Component, inject, signal } from "@angular/core";
+import { Router, RouterLink } from "@angular/router";
+import { WEB_ROUTES } from "../../../../../../core/constants/routes.constants";
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
+import { TokenService } from "../../../../../../core/services/token.service";
+import { RegistrationApiService } from "../../../../../../data/registration.service";
+import { finalize, takeUntil } from "rxjs";
+import { BaseComponent } from "../../../../../../core/base/base.component";
+import { ToastService } from "../../../../../../shared/components/toast/toast.service";
+import { ProfileService } from "../../../../../../data/profile.service";
+import { TranslatePipe, TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-sign-in',
+  selector: "app-sign-in",
   imports: [RouterLink, ReactiveFormsModule, TranslatePipe],
-  templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.scss'
+  templateUrl: "./sign-in.component.html",
+  styleUrl: "./sign-in.component.scss",
 })
 export class SignInComponent extends BaseComponent {
   WEB_ROUTES = WEB_ROUTES;
@@ -29,55 +34,63 @@ export class SignInComponent extends BaseComponent {
   form: FormGroup = this.fb.group({
     userName: [null, Validators.required],
     password: [null, Validators.required],
-    rememberMe: true
-  })
+    rememberMe: true,
+  });
 
   onSignIn(): void {
     if (this.form.valid) {
-      this.loading.set(true)
-      this.registrationService.signIn(this.form.value).pipe(
-        takeUntil(this.destroy$),
-        finalize(() => this.loading.set(false)),
-      ).subscribe({
-        next: (response: any) => {
-          if (response.status !== 200) {
-            if (response.message === 'Incorrect data') {
-              this.toastService.show({ text: this.translateService.instant('AUTHENTICATION.SIGN_IN.INCORRECT_DATA'), classname: 'bg-danger text-light' });
-
-            } else {
-              this.toastService.show({ text: response.message, classname: 'bg-danger text-light' });
+      this.loading.set(true);
+      this.registrationService
+        .signIn(this.form.value)
+        .pipe(
+          takeUntil(this.destroy$),
+          finalize(() => this.loading.set(false))
+        )
+        .subscribe({
+          next: (response: any) => {
+            if (response.status !== 200) {
+              if (response.message === "Incorrect login information") {
+                this.toastService.show({
+                  text: this.translateService.instant(
+                    "AUTHENTICATION.SIGN_IN.INCORRECT_DATA"
+                  ),
+                  classname: "bg-danger text-light",
+                });
+              } else {
+                this.toastService.show({
+                  text: response.message,
+                  classname: "bg-danger text-light",
+                });
+              }
+              return;
             }
-            return
-          }
-          this.tokenService.setToken(response.data);
-          this.getUserProfile();
-        },
-        error: (err) => {
-
-        }
-      })
+            this.tokenService.setToken(response.data);
+            this.getUserProfile();
+          },
+          error: (err) => {},
+        });
     } else {
       this.form.markAllAsTouched();
     }
   }
 
   getUserProfile(): void {
-    this.profileService.getUserProfile().pipe(
-      takeUntil(this.destroy$),
-      finalize(() => this.loading.set(false)),
-    ).subscribe({
-      next: (response) => {
-        this.tokenService.setUser(response.data);
-        if (history.state['redirectionUrl']) {
-          this.router.navigateByUrl(history.state['redirectionUrl'])
-        } else {
-          this.router.navigate(['/' + WEB_ROUTES.DASHBOARD.ROOT]);
-        }
-      },
-      error: (err) => {
-
-      }
-    })
+    this.profileService
+      .getUserProfile()
+      .pipe(
+        takeUntil(this.destroy$),
+        finalize(() => this.loading.set(false))
+      )
+      .subscribe({
+        next: (response) => {
+          this.tokenService.setUser(response.data);
+          if (history.state["redirectionUrl"]) {
+            this.router.navigateByUrl(history.state["redirectionUrl"]);
+          } else {
+            this.router.navigate(["/" + WEB_ROUTES.DASHBOARD.ROOT]);
+          }
+        },
+        error: (err) => {},
+      });
   }
-
 }
