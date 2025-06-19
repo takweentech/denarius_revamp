a4510e55cd79761f83bc7f0d2174e18813be51a4
muhammed loay <70457297+muhammed-loay@users.noreply.github.com>
2025-06-19 06:41:16 +0300

    integrate profile image API in header and profile page

--- src/app/layout/admin-layout/components/header/header.component.ts
+++ src/app/layout/admin-layout/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from "@angular/core";
+import { Component, OnInit, inject, signal } from "@angular/core";
 import { Router, RouterLink } from "@angular/router";
 import { NgbDropdownModule } from "@ng-bootstrap/ng-bootstrap";
 import { WEB_ROUTES } from "../../../../core/constants/routes.constants";
@@ -6,17 +6,24 @@ import { TokenService } from "../../../../core/services/token.service";
 import { TranslationService } from "../../../../core/services/translation.service";
 import { TranslatePipe } from "@ngx-translate/core";
 import { SidebarService } from "../sidebar/sidebar.service";
-import { ConfigService } from '../../../../core/services/config.service';
-import { environment } from '../../../../../environments/environment';
-import { UserProfileData } from '../../../../core/models/user';
+import { ConfigService } from "../../../../core/services/config.service";
+import { environment } from "../../../../../environments/environment";
+import { UserProfileData } from "../../../../core/models/user";
+import { ProfileService } from "../../../../data/profile.service";
+import { takeUntil } from "rxjs";
+import { ToastService } from "../../../../shared/components/toast/toast.service";
+import { BaseComponent } from "../../../../core/base/base.component";
+
 @Component({
   selector: "app-header",
   imports: [NgbDropdownModule, TranslatePipe, RouterLink],
   templateUrl: "./header.component.html",
   styleUrl: "./header.component.scss",
 })
-export class HeaderComponent {
+export class HeaderComponent extends BaseComponent implements OnInit {
   public WEB_ROUTES = WEB_ROUTES;
+  private toastService = inject(ToastService);
+
   translationService = inject(TranslationService);
   private readonly tokenService = inject(TokenService);
   private readonly sidebarService = inject(SidebarService);
@@ -25,9 +32,28 @@ export class HeaderComponent {
   APP_CONFIG = this.configService.getConfig();
   cmsAssetsUrl = environment.cmsAssetsUrl;
   user: UserProfileData = this.tokenService.getUser();
+  private readonly profileService = inject(ProfileService);
+  imageUrl = signal<string>("https://i.ibb.co/MBtjqXQ/user.png"); // fallback avatar
 
   onToggleSidebar() {
     this.sidebarService.toggleSidebar();
+  }
+
+  ngOnInit(): void {
+    // Get image
+    this.profileService
+      .getImageProfile()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if (response.data) {
+            this.imageUrl.set(response.data); // assuming the API returns a full URL
+          }
+        },
+        error: () => {
+          this.imageUrl.set("assets/images/mewees.svg"); // fallback
+        },
+      });
   }
 
   onLogout() {
--- src/app/presentation/admin/pages/settings/components/profile/profile.component.ts
+++ src/app/presentation/admin/pages/settings/components/profile/profile.component.ts
@@ -1,9 +1,24 @@
-import { Component, inject, OnInit, signal } from "@angular/core";
+import {
+  Component,
+  ElementRef,
+  inject,
+  OnInit,
+  signal,
+  ViewChild,
+} from "@angular/core";
 import { TranslatePipe } from "@ngx-translate/core";
-import { UserBasicProfileData, UserProfileData } from "../../../../../../core/models/user";
+import {
+  UserBasicProfileData,
+  UserProfileData,
+} from "../../../../../../core/models/user";
 import { TokenService } from "../../../../../../core/services/token.service";
 import { DatePipe } from "@angular/common";
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
 import { ProfileService } from "../../../../../../data/profile.service";
 import { finalize, takeUntil } from "rxjs";
 import { BaseComponent } from "../../../../../../core/base/base.component";
@@ -16,6 +31,9 @@ import { ToastService } from "../../../../../../shared/components/toast/toast.se
   styleUrl: "./profile.component.scss",
 })
 export class ProfileComponent extends BaseComponent implements OnInit {
+  @ViewChild("fileInput") fileInput!: ElementRef<HTMLInputElement>;
+  selectedFile: File | null = null;
+  imageUrl = signal<string>("https://i.ibb.co/MBtjqXQ/user.png"); // fallback avatar
   private readonly tokenService = inject(TokenService);
   private readonly profileService = inject(ProfileService);
   private readonly fb = inject(FormBuilder);
@@ -25,14 +43,37 @@ export class ProfileComponent extends BaseComponent implements OnInit {
 
   form!: FormGroup;
 
+  triggerFileInput(): void {
+    this.fileInput.nativeElement.click();
+  }
   ngOnInit(): void {
     this.initForm();
-    this.profileService.getBasicPersonalInformation().pipe(takeUntil(this.destroy$)).subscribe({
-      next: (response) => {
-        this.user.set(response.data);
-        this.initForm(response.data);
-      }
-    })
+
+    // Get address data
+    this.profileService
+      .getBasicPersonalInformation()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          this.user.set(response.data);
+          this.initForm(response.data);
+        },
+      });
+
+    // Get image
+    this.profileService
+      .getImageProfile()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if (response.data) {
+            this.imageUrl.set(response.data); // assuming the API returns a full URL
+          }
+        },
+        error: () => {
+          this.imageUrl.set("assets/images/mewees.svg"); // fallback
+        },
+      });
   }
 
   initForm(data?: UserBasicProfileData) {
@@ -42,30 +83,91 @@ export class ProfileComponent extends BaseComponent implements OnInit {
       city: [data?.city, Validators.required],
       postalCode: [data?.postalCode, Validators.required],
       additionalCode: [data?.additionalCode, Validators.required],
-      shortAddress: "TEMP"
-    })
+      shortAddress: "TEMP",
+    });
   }
+  onImageSelected(event: Event): void {
+    const fileInput = event.target as HTMLInputElement;
+    if (!fileInput.files?.length) return;
 
-  onSave() {
+    const file = fileInput.files[0];
+    this.selectedFile = file; // Store for saving later
+
+    // Preview the selected image
+    const objectUrl = URL.createObjectURL(file);
+    this.imageUrl.set(objectUrl); // Update local preview only
+  }
+  onSave(): void {
     if (this.form.invalid) {
       this.form.markAllAsTouched();
-      return
-    };
+      return;
+    }
+
     this.loading.set(true);
 
-    this.profileService.saveBasicPersonalInformation(this.form.value).pipe(
-      finalize(() => this.loading.set(false)),
-      takeUntil(this.destroy$)).subscribe({
-        next: (response) => {
-          if (response.status == 200) {
-            this.toastService.show({ text: "Address was successfully saved", classname: 'bg-success text-light', icon: 'fa-circle-check' });
-          } else {
-            this.toastService.show({ text: response.message, classname: 'bg-danger text-light', icon: 'fa-circle-exclamation' });
-          }
-        },
-        error: (error) => {
+    const saveForm = () => {
+      this.profileService
+        .saveBasicPersonalInformation(this.form.value)
+        .pipe(
+          finalize(() => this.loading.set(false)),
+          takeUntil(this.destroy$)
+        )
+        .subscribe({
+          next: (response) => {
+            this.toastService.show({
+              text:
+                response.status === 200
+                  ? "Address was successfully saved"
+                  : response.message,
+              classname:
+                response.status === 200
+                  ? "bg-success text-light"
+                  : "bg-danger text-light",
+              icon:
+                response.status === 200
+                  ? "fa-circle-check"
+                  : "fa-circle-exclamation",
+            });
+          },
+          error: () => {
+            this.toastService.show({
+              text: "Failed to save address",
+              classname: "bg-danger text-light",
+              icon: "fa-circle-exclamation",
+            });
+          },
+        });
+    };
 
-        }
-      })
+    // ✅ Upload the image using ONLY the file name
+    if (this.selectedFile) {
+      const fileName = this.selectedFile.name;
+
+      this.profileService
+        .setImageProfile(fileName)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: (res) => {
+            if (res.status === 200) {
+              this.toastService.show({
+                text: "Profile image updated",
+                classname: "bg-success text-light",
+                icon: "fa-circle-check",
+              });
+            }
+            saveForm(); // Continue saving after image update
+          },
+          error: () => {
+            this.toastService.show({
+              text: "Image upload failed",
+              classname: "bg-danger text-light",
+              icon: "fa-circle-exclamation",
+            });
+            this.loading.set(false);
+          },
+        });
+    } else {
+      saveForm();
+    }
   }
 }
