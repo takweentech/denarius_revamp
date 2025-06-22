8fe0e3abcee95b07427e2ffc5fc18ea2751d59a8
muhammed loay <70457297+muhammed-loay@users.noreply.github.com>
2025-06-23 02:43:36 +0300

    add back button to investments details

--- src/app/presentation/admin/pages/investments/components/details/details.component.ts
+++ src/app/presentation/admin/pages/investments/components/details/details.component.ts
@@ -1,19 +1,24 @@
-import { Component, inject } from '@angular/core';
-import { Investment } from '../../../../../../core/models/investment';
-import { ActivatedRoute } from '@angular/router';
-import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
-import { TranslatePipe } from '@ngx-translate/core';
-import { DatePipe } from '@angular/common';
+import { Component, inject } from "@angular/core";
+import { Investment } from "../../../../../../core/models/investment";
+import { ActivatedRoute } from "@angular/router";
+import { NgbNavModule } from "@ng-bootstrap/ng-bootstrap";
+import { TranslatePipe } from "@ngx-translate/core";
+import { DatePipe } from "@angular/common";
+import { Location } from "@angular/common";
 
 @Component({
-  selector: 'app-details',
+  selector: "app-details",
   imports: [NgbNavModule, TranslatePipe, DatePipe],
-  templateUrl: './details.component.html',
-  styleUrl: './details.component.scss'
+  templateUrl: "./details.component.html",
+  styleUrl: "./details.component.scss",
 })
 export class DetailsComponent {
+  private location = inject(Location);
+
   private readonly activatedRoute = inject(ActivatedRoute);
   active: number = 1;
-  investment: Investment = this.activatedRoute.snapshot.data['investment'].data;
-
+  investment: Investment = this.activatedRoute.snapshot.data["investment"].data;
+  goBack(): void {
+    (this.location as any).back();
+  }
 }
