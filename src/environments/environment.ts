05d9dc993ccae67ed11e2507ec3a20bfc975f8eb
zatattou <z.atattou@takweentech.sa>
2025-07-04 16:56:01 +0100

    update build action

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
   apiUrl: 'https://sharikekapi.takweentech.sa/api/v1',
   cmsUrl: 'https://sharikekcms.paramej.com/api',
   cmsAssetsUrl: 'https://sharikekcms.paramej.com/',
-  version: '4.5',
+  version: '4.6',
 } as const;
