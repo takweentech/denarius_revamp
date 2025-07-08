36885aeedb0eb8d35df4592475edcbf56d537596
muhammed loay <m.loay@takweentech.sa>
2025-07-08 05:41:31 +0300

    add router link to contact us button on services page

--- src/app/presentation/landing/pages/services/components/listing/components/contact/contact.component.ts
+++ src/app/presentation/landing/pages/services/components/listing/components/contact/contact.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { BaseComponent } from '../../../../../../../../core/base/base.component';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 import { takeUntil } from 'rxjs';
 import { StrapiService } from '../../../../../../../../core/strapi/strapi.service';
+import { WEB_ROUTES } from '../../../../../../../../core/constants/routes.constants';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
 export class ContactComponent extends BaseComponent implements OnInit {
+  WEB_ROUTES = WEB_ROUTES;
+
   private readonly activatedRoute = inject(ActivatedRoute);
   content = this.activatedRoute.snapshot.data['content']['contact'];
   private readonly strapiService = inject(StrapiService);
@@ -23,14 +26,17 @@ export class ContactComponent extends BaseComponent implements OnInit {
   ngOnInit(): void {
     this.translateService.onLangChange.pipe(takeUntil(this.destroy$)).subscribe((event: LangChangeEvent) => {
       this.getContent();
-    })
-  };
+    });
+  }
 
   getContent(): void {
-    this.strapiService.getContentByPage(`/service?locale=${this.translateService.currentLang}&populate=contact`).pipe(takeUntil(this.destroy$)).subscribe({
-      next: (response) => {
-        this.content = response.contact
-      }
-    })
+    this.strapiService
+      .getContentByPage(`/service?locale=${this.translateService.currentLang}&populate=contact`)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: response => {
+          this.content = response.contact;
+        },
+      });
   }
 }
