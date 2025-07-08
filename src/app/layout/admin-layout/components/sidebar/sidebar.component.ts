a0d4bf0005ca5dc8f67affdee7790d8df7eeb054
zatattou <z.atattou@takweentech.sa>
2025-07-08 15:10:42 +0100

    add contact option to layout

--- src/app/layout/admin-layout/components/sidebar/sidebar.component.ts
+++ src/app/layout/admin-layout/components/sidebar/sidebar.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SidebarService } from './sidebar.service';
 import { TranslatePipe } from '@ngx-translate/core';
+import { WEB_ROUTES } from '../../../../core/constants/routes.constants';
 
 @Component({
   selector: 'app-sidebar',
@@ -12,4 +13,6 @@ import { TranslatePipe } from '@ngx-translate/core';
 export class SidebarComponent {
   private readonly sidebarService = inject(SidebarService);
   sidebarItems = this.sidebarService.sidebarItems;
+  public WEB_ROUTES = WEB_ROUTES;
+
 }
