2a1de5f8f89082f56edb1b9fec06b8cabb78003b
zatattou <z.atattou@takweentech.sa>
2025-07-08 11:29:12 +0100

    uninstall marked package

--- src/app/presentation/landing/pages/terms/terms.component.ts
+++ src/app/presentation/landing/pages/terms/terms.component.ts
@@ -1,26 +1,21 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { BaseComponent } from '../../../../core/base/base.component';
 import { ActivatedRoute } from '@angular/router';
-import { DomSanitizer } from '@angular/platform-browser';
-import { marked } from 'marked';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 import { StrapiService } from '../../../../core/strapi/strapi.service';
-import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import { MarkdownComponent } from 'ngx-markdown';
 
 @Component({
   selector: 'app-terms',
-  imports: [],
+  imports: [MarkdownComponent],
   templateUrl: './terms.component.html',
   styleUrl: './terms.component.scss',
 })
 export class TermsComponent extends BaseComponent implements OnInit {
   private readonly activatedRoute = inject(ActivatedRoute);
-  private readonly sanitizer = inject(DomSanitizer);
   private readonly translateService = inject(TranslateService);
   private readonly strapiService = inject(StrapiService);
-
-  override destroy$ = new Subject<void>();
 
   content = this.activatedRoute.snapshot.data['content']['terms_content'];
 
@@ -29,17 +24,10 @@ export class TermsComponent extends BaseComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.parseMarkdown();
 
     this.translateService.onLangChange.pipe(takeUntil(this.destroy$)).subscribe((event: LangChangeEvent) => {
       this.getContent();
     });
-  }
-
-  parseMarkdown(): void {
-    const rawMarkdown = this.content?.terms_and_conditions_content || '';
-    const html: string = marked.parse(rawMarkdown) as string;
-    this.content.terms_and_conditions_content = this.sanitizer.bypassSecurityTrustHtml(html);
   }
 
   getContent(): void {
@@ -49,13 +37,7 @@ export class TermsComponent extends BaseComponent implements OnInit {
       .subscribe({
         next: response => {
           this.content = response.terms_content;
-          this.parseMarkdown();
         },
       });
-  }
-
-  override ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
   }
 }
