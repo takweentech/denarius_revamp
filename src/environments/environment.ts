09cbd51efe9832fd643c616fdcc980c88bf84841
zatattou <z.atattou@takweentech.sa>
2025-07-09 15:52:19 +0100

    adjust previous icon

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
   apiUrl: 'https://sharikekapi.takweentech.sa/api/v1',
   cmsUrl: 'https://sharikekcms.paramej.com/api',
   cmsAssetsUrl: 'https://sharikekcms.paramej.com/',
-  version: '5.3',
+  version: '5.4',
 } as const;
